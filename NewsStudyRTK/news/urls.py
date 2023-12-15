1951258e8f15b47c6d58c19735f8e748ae0e5f5d
dan-sao <dan-sao@mail.ru>
2023-12-15 17:10:14 +0300

    n+1 commit #ничего не успел

--- NewsStudyRTK/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-urlpatterns = [
-    path('show/', views.index, name='news_index'),
-    path('show/<int:id>', views.detail, name='news_detail'),
-]
